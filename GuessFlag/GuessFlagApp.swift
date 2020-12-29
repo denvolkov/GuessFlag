4b87e6e4046c0cb28c72ed8ed67ce9516cff51c0
denvolkov <denvolkov@inbox.ru>
2020-12-29 15:50:42 +0300

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by test on 29.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
